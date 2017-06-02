c00bee62de02415d1512ecb59890bb9dc7045dfe
adityars-sidqi <adityars.sidqi@gmail.com>
2017-06-03 06:58:47 +0700

    chat v1.0.0

---
+++ server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  //kalau ada message baru
+  socket.on('newMessage', function(msg) {
+    io.emit('newMessage', msg);
+    console.log('Chat baru: ' + msg);
+  });
+
+  //kalau user disconnect
+  socket.on('disconnect', function(msg) {
+    console.log('User disconnected');
+  });
+});
+
+http.listen(3003, function() {
+  console.log('listening on 3000..');
+});
